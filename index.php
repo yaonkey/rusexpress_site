f72f7b783569aea7e47a3590b1950497831088dd
Yaonkey <thesuperuserstyle@gmail.com>
2021-03-31 17:27:21 +0300

    Видоизменен калькулятор

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
-echo "Rusexpress calculator";
+echo "<h2>Rusexpress calculator</h2>";
 include "modules/Calculator.php";
-$calc = new Calculator();
 
+$calc = new Calculator();
+//echo $calc->Test("Краснодар");
+$temp = $calc->GetAllCities();
+
+foreach ($temp as $city){
+    echo "<input type='submit' name='but_{$counter}' value='{$city}'/>";
+    $counter++;
+}
--- modules/Calculator.php
+++ modules/Calculator.php
@@ -3,101 +3,90 @@
 class Calculator
 {
     private float $cost; // Общая стоимость
-    private array $services; // Список использованных услуг
-    private array $ServicesList; // Список всех услуг и их стоимость
+    private array $usedServices; // Список использованных услуг
+    private array $Services; // Список всех услуг и их стоимость
+    private array $Cities; // Список всех городов и сроки доставки
+    private array $Zones; // Список зон
 
-    function __construct(){
-//        $this->cost=$cost;
-        $this->ServicesList = include "components/Services.php";
+    public function __construct()
+    {
+        $this->Services = include "components/Services.php";
+        $this->Cities = include "components/Cities.php";
+        $this->Zones = include "components/Zones.php";
     }
 
     /**
-     * Добавляет название услуги в список использованных услуг
-     * Прибавляет стоимость услуги в общую стоимость
-     * @param string $service Сервис, который нужно включить в стоимость
-     * @return bool
+     * Доставка по Москве и МО (за МКАД)
+     * @param string $region Москва или МО
+     * @param string $sendType Срочно или Эконом
+     * @param int $weight Вес заказа
      */
-    public function Add(string $service): bool
+    public function MoscowSend(string $region, string $sendType, int $weight){
+        if ($region == 'Москва' || $region == 'МО'){
+            if ($sendType == 'Эконом' || $sendType == 'Срочно'){
+                if ($weight > 0){
+                    // todo: Сделать доставку по Москве и МО
+                }else{
+                    echo "Вес должен быть больше 0";
+                }
+            }else{
+                echo "Выберите тип доставки: Эконом или Срочно";
+            }
+        }else{
+            echo "Выберите регион: Москва (в пределах МКАД) или МО (за пределами МКАД)";
+        }
+    }
+
+    public function ChangeCity(string $city){
+        if ($this->CityInServices($city)){
+            if ($this->Services[0] == 'Москва Мкад'){
+                // todo
+            }
+        }
+    }
+
+    /**
+     * Изменяемая функция для тестирования модулей
+     */
+    public function Test(string $city){
+        return $this->GetCityZone($city);
+    }
+
+    public function GetAllCities(): array
     {
-        if (in_array($service, $this->ServicesList)) {
-            $this->cost += $this->ServicesList[$service];
-            array_push($this->services, $service);
+        return array_keys($this->Cities);
+    }
+
+    /**
+     * Находит зону города
+     * @param string $city Искомый город
+     * @return int Зона города
+     */
+    public function GetCityZone(string $city): int
+    {
+        if ($this->CityInServices($city)){
+            foreach ($this->Cities as $cityKey => $cityParams){
+                if ($cityKey == $city && (int)$cityParams[1] <= 10 ){
+                    return $cityParams[1];
+                }
+            }
+        }
+        return 0;
+    }
+
+
+    /**
+     * Проверяет наличие города в базе услуг
+     * @param string $city Проверяемый город
+     * @return bool Наличие города
+     */
+    private function CityInServices(string $city): bool
+    {
+        if (in_array($city, array_keys($this->Cities))){
             return true;
-        }
-        return false;
-    }
-
-    /**
-     * Убирает услугу из списка использованных услуг
-     * Вычитает стоимость услуги из общей стоимости
-     * @param string $service Сервис, который требуется убрать из стоимости
-     * @return bool
-     */
-    public function Remove(string $service): bool{
-
-        if (in_array($service, $this->services)){
-            $this->cost -= $this->services[$service];
-            unset($this->services[$service]);
-            return true;
-        }
-        return false;
-    }
-
-    /* Getters */
-
-    /**
-     * Возвращает стоимость одной услуги
-     * @param string $service Наименование сервиса
-     * @return float
-     */
-    public function GetServiceCost(string $service): float
-    {
-
-        if (in_array($service, $this->ServicesList)){
-            return $this->ServicesList[$service];
+        }else{
+            return false;
         }
     }
 
-    /**
-    * Возвращает общую стоимость
-    * @return float
-    */
-    public function GetCost(): float
-    {
-
-        return $this->cost;
-    }
-
-    /**
-    * Возвращает список используемых услуг
-    * @return array
-    */
-    public function GetUsedServices(): array
-    {
-
-        return $this->services;
-    }
-
-    /**
-     * Возвращает список всех доступных услуг
-     * @return array
-     */
-    public function GetAllServices(): array
-    {
-
-        return $this->ServicesList;
-    }
-
-    /* Render */
-
-    /**
-     * Позволяет отрисовать калькулятор на странице
-     * @return string
-     */
-    public function Render(): string
-    {
-
-
-        return "";
-    }
 }
